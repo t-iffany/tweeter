67b3b897f23fb63a351e0dbd5cb3807e4dd0d70c
t-iffany <tiffanyxleong@gmail.com>
2022-12-08 01:17:14 +0000

    -Implement renderTweets function -Remove <article> from index.html -Fixed header icons

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,72 +4,91 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Define a function that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
-// Create HTML markup using template literals
-  const createTweetHeader = function (user) {
-    let $header = $(`<header>`);
-
-    let $avatar = $(`<img src="${user.avatars}">`);
-    let $name = $(`<h3>${user.name}</h3>`);
-    let $handle = $(`<h4>${user.handle}</h4>`);
-
-    $header.append($avatar, $name, $handle);
-
-    return $header;
-  }
-
-  const createTweetBody = function(content) {
-    let $tweetBody = $(`<content>${content.text}</content>`);
-
-    return $tweetBody;
-  }
-
-  const createTweetFooter = function (object) {
-    let $footer = $(`<footer>`);
-
-    let $footerTime = $(`<div class="days-posted">${object.created_at}</div>`);
-    let $footerIcons = $(`<div class="icons"><i class="icons fa-solid fa-flag"><i class="icons fa-sharp fa-solid fa-retweet"><i class="icons fa-solid fa-heart"></div>`);
-   
-    $footer.append($footerTime, $footerIcons);
-
-    return $footer;
-  }
-
-  const createTweetElement = function(tweetData) {
-    let $header = createTweetHeader(tweetData.user);
-    let $tweetBody = createTweetBody(tweetData.content);
-    let $footer = createTweetFooter(tweetData);
-
-    let $tweet = $(`<article class="tweet">`);
-    $tweet.append($header, $tweetBody, $footer);
-    
-    return $tweet;
-  };
-
-
-// The tweet data object that the function will take will have all the necessary tweet data:
-// taken from initial-tweets.json file in the data-files directory
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = 
-  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-  }
-
 $(document).ready(() => {
-  
-const $tweet = createTweetElement(tweetData);
-  // Test / driver code (temporary)
-console.log('test this is the tweet:', $tweet); // to see what it looks like
-$('.list-of-tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
+  renderTweets(tweetData);
+  
 }) 
 
+// Fake data taken from initial-tweets.json
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+// Define a function that takes in an array of tweet objects and appends each one to 'list-of-tweets'
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  for (let tweet of tweets) {
+  // calls createTweetElement for each tweet    
+    const $newTweet = createTweetElement(tweet);
+  // takes return value and appends it to the tweets container
+  $('.list-of-tweets').append($newTweet); 
+  }
+};
+
+
+// Define a function that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet
+ 
+const createTweetElement = function(tweetData) {
+  let $header = createTweetHeader(tweetData.user);
+  let $tweetBody = createTweetBody(tweetData.content);
+  let $footer = createTweetFooter(tweetData);
+
+  let $tweet = $(`<article class="tweet">`);
+  $tweet.append($header, $tweetBody, $footer);
+    
+  return $tweet;
+};
+
+
+// Create HTML markup using template literals
+const createTweetHeader = function(user) {
+  let $header = $(`<header>`);
+
+  let $avatar = $(`<img src="${user.avatars}">`);
+  let $name = $(`<h3>${user.name}</h3>`);
+  let $handle = $(`<h4>${user.handle}</h4>`);
+
+  $header.append($avatar, $name, $handle);
+
+  return $header;
+}
+
+const createTweetBody = function(content) {
+  let $tweetBody = $(`<content>${content.text}</content>`);
+
+  return $tweetBody;
+}
+
+const createTweetFooter = function(object) {
+  let $footer = $(`<footer>`);
+
+  let $footerTime = $(`<div class="days-posted">${object.created_at}</div>`);
+  let $footerIcons = $(`<div class="icons"><i class="fa-solid fa-flag"></i><i class="fa-sharp fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>`);
+   
+  $footer.append($footerTime, $footerIcons);
+
+  return $footer;
+}
