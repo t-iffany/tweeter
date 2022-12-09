38aa2d25061ae920fb041f1e9788c46ac0e8c9eb
t-iffany <tiffanyxleong@gmail.com>
2022-12-09 03:29:41 +0000

    Reformatted composer-char-counter.js and removed commented out code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,19 +2,17 @@
 // $('textarea').val().length will find length of the string
 
 $(document).ready(function() {
-  // --- our code goes here ---
 
-  $('#tweet-text').on('input', function() {  // counts characters of input in textarea#tweet-text
-    // use 'this' to grab the value of the textarea in question and determine the length of that input value
-    let remainingChars = 140 - $(this).val().length;  // $(this).value is the value of the jquery object consisting of the input characters
-    
+  // counts characters of input in textarea#tweet-text
+  $('#tweet-text').on('input', function() {
+    let remainingChars = 140 - $(this).val().length;
+
     // render the result by updating the counter
     let counter = $(this).siblings('.tweet-footer').children('.counter');
-    counter.val(remainingChars);  
+    counter.val(remainingChars);
 
     // counter turns red when 140 character limit exceeded
     counter.toggleClass('red-counter', remainingChars < 0);
   });
 
 });
-
